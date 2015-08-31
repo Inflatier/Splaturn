40279fc571fa6277674ed975b9e3b9b5a70a2a56
猪野 湧斗 <ice.softicon30th@gmail.com>
2015-09-01 01:37:19 +0900

    roomモジュールを作りかけ

---
+++ modules/room.js
@@ -0,0 +1,27 @@
+/**
+ * Room
+ * 部屋のクラス
+ * 
+ * 引数
+ *     id (int) その部屋の一意なID。マップのSVGのIDと対応する。
+ *     name (String) その部屋の名称。
+ *     color (Color) その部屋の初期の色。Color列挙体の色を指定。
+ * 
+ * メンバー変数(個↑々↓のインスタンスが持つ変数)一覧
+ *     id (int) その部屋の一意なID。マップのSVGのIDと対応する。
+ *     name (String) その部屋の名称。
+ *     lockExpiration (int) その部屋のロックの有効期限。ミリ秒表記。0のときロックは解除された状態。
+ *     color (Color) その部屋の色。Color列挙体の色を指定。
+ *     isTrapped (boolean) その部屋にトラップが仕掛けられている(true)か否(false)か。
+ * 
+ */
+ 
+ function Room(id, name, color) {
+	 this.id = id;
+	 this.name = name;
+	 this.lockExpiration = 0;
+	 this.color = color;
+	 this.isTrapped = false;
+ }
+ 
+ module.exports = Room;
