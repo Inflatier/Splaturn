dceb3996948f35e2280baac131a1f98845b0ff56
猪野 湧斗 <ice.softicon30th@gmail.com>
2015-09-06 22:48:49 +0900

    base

--- splaturn.js
+++ splaturn.js
@@ -1,6 +1,19 @@
 var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
 var app = express();
 
+app.use(cookieParser());
+app.use(session({secret: 'YSFH情報工学部'}));
 app.use(express.static(__dirname + '/public'));
+
+(function initializeRooms() {
+	
+})();
+
+app.get('/', function (req, res) {
+	res.end('Hello World');
+});
 
 app.listen(14514);
