187c78b657bd30fbfb5fad7e41d22e278ca0dc1d
猪野 湧斗 <ice.softicon30th@gmail.com>
2015-09-13 23:33:51 +0900

    api.qrserver.comのプロキシができました。

--- splaturn.js
+++ splaturn.js
@@ -1,3 +1,4 @@
+var proxy = require('express-http-proxy');
 var express = require('express');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
@@ -58,6 +59,12 @@ var entry = require('./routes/entry');
 var endpoints = require('./routes/endpoints')(app);
 
 // APIのエンドポイントたち
+app.get('/entry', function (req, res) {
+	res.sendFile(__dirname + '/public/entry.html');
+});
+app.get('/game', function (req, res) {
+	res.sendFile(__dirname + '/public/game.html');
+});
 app.post('/join', entry.join);
 app.get('/quit', entry.quit);
 
@@ -70,7 +77,7 @@ app.post('/trap', endpoints.trap);
 
 app.get('/', function index(req, res) {
 	if (req.session.entried == true) res.redirect('/game');
-	else res.render('index');
+	else res.redirect('/entry');
 });
 
 app.get('/game', function game(req, res) {
@@ -80,6 +87,11 @@ app.get('/game', function game(req, res) {
 app.get('/paint', function paint(req, res) {
 	res.render('paint');
 });
+
+
+app.use('/qr', proxy('api.qrserver.com', {
+  forwardPath: function(req, res) { return '/v1/read-qr-code/'; }
+}));
 
 // 静的なファイルを提供するモジュール
 app.use(express.static(__dirname + '/public'));
