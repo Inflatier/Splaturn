10e998690b6c0d05c324a1f72700652a5d9294c6
Jade0515 <Jade0515>
2015-09-15 12:48:48 +0900

    マップ改良

--- public/init.js
+++ public/init.js
@@ -10,8 +10,11 @@ var mapdiv = body.append("div");
 
 var timep = body.append("p");
 
+var scopefloor = 3;
+
 function redisplay(){
 	
+	//時間描画
 	timep.style({
 		width: "100%",
 		color:"white",
@@ -20,65 +23,11 @@ function redisplay(){
 		"margin-top":"140px",
 		"text-align": "center",
 	}).text(function(){
-		return "マップ（残り "+Math.floor(lefttime/60) + "分" + Math.floor(lefttime%60) + "秒）";
+		return "（残り "+Math.floor(lefttime/60) + "分" + Math.floor(lefttime%60) + "秒）";
 	});
 	
-	mapdiv.selectAll("div").data(map).enter().append("div").on("click",function(d,i){
-
-		boad.selectAll("div").remove();
-
-		mapdiv.selectAll("div").style({
-			color:"white",
-			background:"gray",
-		});
-		boad.style({
-			background:"white",
-		})
-		mapdiv.select("div:nth-of-type("+(i+1)+")").style({
-			color:"black",
-			background:"white",
-		});
-
-		boad.append("div").html(mapsvg[i]);
-		/*selectAll("div").data(map[i]).enter().append("div").style({
-			position:"absolute",
-			"margin-left":function(e,n){return 10+n%3*90+"px";},
-			"margin-top":function(e,n){return 7+Math.floor(n/3)*87+"px";},
-			width:"80px",
-			height:"80px",
-			border: "2px solid black",
-			background:function(e,n){return e.color;},
-			"text-align":"center",
-			"line-height":"80px",
-			color:function(e,n){
-				if(e.color=='')return "black";
-			},
-		}).text(function(e,n){
-			return e.name;
-		});*/
-
-	}).style({
-		color:"white",
-		"font-size":"20px",
-		position:"absolute",
-		height:"40px",
-		width:"15%",
-		"margin-top":function(d,i){return 160+40*(i+1)},
-		"margin-left":"5%",
-		background:"gray",
-		"text-align":"center",
-		"line-height":"40px",
-	}).text(function(d,i){return "F"+(i+1)});
-
-	var boad = mapdiv.append("div").style({
-		background:"white",
-		position:"absolute",
-		"margin-left":"19%",
-		"margin-top":"200px",
-		width:"70%",
-		height:"200px",
-	});
-
+	//マップ描画（スコープに応じたものを再描画）
+	
 	
 }
 
@@ -112,7 +61,7 @@ request
   .get("/rooms")
   .end(function(err, res){
 	
-	//map=res;
+	map=res;
 	
   });
 request
@@ -127,6 +76,70 @@ request
 
 
 
+
+//map作成
+for(var i=3;i<=5;i++){
+	mapdiv.append("div").on("click",function(d,i){
+
+		scopefloor=this.id;
+
+		mapdiv.selectAll("div").style({
+			color:"white",
+			background:"gray",
+		});
+		boad.style({
+			background:"white",
+		})
+		mapdiv.select("div:nth-of-type("+(scopefloor-2)+")").style({
+			color:"black",
+			background:"white",
+		});
+
+		//boad.append("div").html(mapsvg[i]);
+		/*selectAll("div").data(map[i]).enter().append("div").style({
+			position:"absolute",
+			"margin-left":function(e,n){return 10+n%3*90+"px";},
+			"margin-top":function(e,n){return 7+Math.floor(n/3)*87+"px";},
+			width:"80px",
+			height:"80px",
+			border: "2px solid black",
+			background:function(e,n){return e.color;},
+			"text-align":"center",
+			"line-height":"80px",
+			color:function(e,n){
+				if(e.color=='')return "black";
+			},
+		}).text(function(e,n){
+			return e.name;
+		});*/
+
+		}).style({
+			color:"white",
+			"font-size":"20px",
+			position:"absolute",
+			height:"40px",
+			width:"15%",
+			"margin-top":function(){return 160+40*(i-2)},
+			"margin-left":"5%",
+			background:"gray",
+			"text-align":"center",
+			"line-height":"40px",
+		}).attr({
+			id:i,
+		}).text(function(){return "F"+(i)});
+}
+var boad = mapdiv.append("div").style({
+	background:"white",
+	position:"absolute",
+	"margin-left":"19%",
+	"margin-top":"200px",
+	width:"70%",
+	height:"200px",
+});
+
+
+
+
 //data更新
 setInterval(function(){
 	
@@ -134,7 +147,7 @@ setInterval(function(){
   .get("/rooms")
   .end(function(err, res){
 	
-	//map=res;
+	map=res;
 	
   });
 	
