e46efa176682a867ee71f4b7288968c2c3a61c3d
Jade0515 <Jade0515>
2015-09-18 18:52:01 +0900

    picture

--- modules/player.js
+++ modules/player.js
@@ -1,7 +1,7 @@
 function Player(id, name, color) {
 	this.name = name;
 	this.id = id;
-	this.items = [""];
+	this.items = ["trap","nullPeinter","locker"];
 	this.color = color;
 	this.paintedRooms = [];
 	this.notifications = [];
